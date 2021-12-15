a52c949cf3051e42ec1b9965dd665770b7db6825
Ferhat Eroglu <ferhateroglu1907@gmail.com>
2021-12-15 19:22:26 +0300

    add async await

--- src/app.js
+++ src/app.js
@@ -8,56 +8,75 @@ const app = express();
 app.use(express.json());//post tan json almak için kullanılır
 
 //get users
-app.get('/users',(req,res) =>{
-    User.find({}).then(users => res.status(200).send(users)).catch(e => res.status(500)); 
+app.get('/users', async (req,res) =>{
+    const users = await User.find({});
+    try{
+        res.status(200).send(users)
+    }catch(e){
+        res.status(500)
+    }
 });
 
-//get users/:id //get request de body olmaz params. olur
-app.get('/users/:id',(req,res) =>{
-    //console.log(req.params);// /users/123 sorgusunu döner
-    //console.log(req.query);// /users/:id=1234 sorgusunu döner
+//get users/:id //get request de body olmaz params. olur //console.log(req.params);// /users/123 sorgusunu döner //console.log(req.query);// /users/:id=1234 sorgusunu döner
+app.get('/users/:id', async (req,res) =>{
     const { id }  = req.params;
-
-    User.findById(id)
-    .then(user => {
+    try{
+        const user = await User.findById(id);
         if(user){
             res.status(200).send(user);
         }else{
-            res.status(400).send();
+            res.status(404).send();
         }
-    })
-    .catch(e => res.status(500).send(e));
-    
+    }catch(e){
+        res.status(500).send(e);
+    }
 });
 
 // list all tasks
-app.get('/tasks',(req,res) =>{
-    Task.find({}).then(t =>res.status(200).send(t)).catch(e => res.status(500).send(e));    
+app.get('/tasks', async (req,res) =>{
+    try{
+        const t = await Task.find({});
+        res.status(200).send(t);
+    }catch(e){
+        res.status(500).send(e);
+    }
 });
 
-// find task
-
-app.get('/tasks/:id',(req,res) =>{
+// list task
+app.get('/tasks/:id', async (req,res) =>{
     const {id} = req.params;
-    Task.findById(id).then(t =>{
+    try{
+        const t = await Task.findById(id);
         if(t){
             res.status(200).send(t);
         }else{
             res.status(400).send();
         }
-    }).catch(e => res.status(500).send(e));
+    }catch(e){
+        res.status(500).send(e);
+    }
 });
 
 //get user
 
-app.post('/tasks',(req, res)=>{
+app.post('/tasks', async (req, res)=>{
     const task = new Task(req.body);
-    task.save().then(t =>{res.send(t);}).catch(e => {res.send(e);});
+    try{
+        const t = await user.save();
+        res.send(t);
+    }catch(e){
+        res.send(e);
+    }
 });
 
-app.post('/users',(req,res) =>{
+app.post('/users', async (req,res) =>{
     const user = new User(req.body);
-    user.save().then((u) =>{res.status(201).send(u);}).catch((e) =>{res.status(400).send(e);});
+    try{
+        const u = await user.save();
+        res.status(201).send(u);
+    }catch(e){
+        res.status(400).send(e);
+    }
 });
 
 app.listen(process.env.PORT || 3000, () =>{
